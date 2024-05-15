3d6e5fd1a091d897bff94e7ae72b16842e510609
Marvin <153880760+smarv2023@users.noreply.github.com>
2024-05-14 22:43:06 -0500

    Update

--- Recursive/Program.cs
+++ Recursive/Program.cs
@@ -1,37 +1,56 @@
-//Fibonacci O(n2)
+//Fibonacci O(n)
 
-using System.Reflection.Emit;
-
-int n = 50;
+int n = 1000;
 double[] memo = new double[n + 1];
 /* 
 for (int i = 0; i <= n; i++)
 {
-    memo[i] = fib(i, memo);
-    Console.WriteLine($"index {i} :: {memo[i]}");
+    memo [i] = fibMemoization(i, memo);
+    Console.WriteLine(i + ": " + memo[i]);
 }
- */
+*/
 
- Console.WriteLine(fib(n, memo));
+ Console.WriteLine($"Memoization: nth: {n} ::{fibMemoization(n, memo)}");
 
-double fib(int n, double[] memo)
+ Console.WriteLine($"Tabular nth: {n} ::{fibTabular(n)}");
+
+//============================================================================================================
+// fib fuction memoization
+double fibMemoization(int n, double[] memo)
 {
-    Console.WriteLine($"New cycle {n}");
     if (n <= 1)
     {
-        Console.WriteLine($"if return {n}");
         return n;
     }
     else if (memo[n] != 0)
         {
-            Console.WriteLine($"else if {n} :: {memo[n]}");
             return memo[n]; // Return memoized value if available
         }
     else
     {
-        Console.WriteLine($"else {n} :: {memo[n]}");
-        memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+        memo[n] = fibMemoization(n - 1, memo) + fibMemoization(n - 2, memo);
         return memo[n];
     }
+}
 
+//============================================================================================================
+// fib function Tabular
+double fibTabular(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        return 1;
+    }
+
+    double[] bottomUp = new double[n + 1];
+    bottomUp[0] = 0;
+    bottomUp[1] = 1;
+    bottomUp[2] = 1;
+
+    for (int i = 3; i <= n; i++)
+    {
+        bottomUp[i] = bottomUp[i - 1] + bottomUp[i - 2];
+    }
+
+    return bottomUp[n];
 }
