81021d0f5f83c48802748d468afff60a12c7723a
Marvin <153880760+smarv2023@users.noreply.github.com>
2024-05-26 02:22:50 -0500

    update

---
+++ Calculator/Program.cs
@@ -0,0 +1,65 @@
+﻿string? readInput;
+int firstNumber;
+int secondNumber;
+
+
+Console.WriteLine("Calculator");
+firstNumber = inputValidator("first");
+secondNumber = inputValidator("second");
+Calculator(firstNumber, secondNumber);
+
+
+void Calculator(int firstNumber, int secondNumber)
+{
+    decimal answer = 0;
+    readInput = "";
+    Console.Write("Select Operator (+, -, *, /): "); 
+    while (readInput == "")
+    {
+        readInput = Console.ReadLine();
+        switch (readInput)
+        {
+            case "+":
+                answer = firstNumber + secondNumber;
+                break;
+            case "-":
+                answer = firstNumber - secondNumber;
+                break;
+            case "*":
+                answer = firstNumber * secondNumber;
+                break;
+            case "/":
+                if (secondNumber == 0)
+                {
+                    Console.WriteLine("Invalid Operation cannot devided by 0: ");
+                    readInput = "";
+                }
+                else
+                    answer = (decimal)firstNumber / secondNumber;
+                break;
+            default:
+                Console.Write("Please select from operators (+, -, *, /): ");
+                readInput = "";
+                break;
+        }
+    }
+    Console.WriteLine($"{firstNumber} {readInput} {secondNumber} = {answer}");
+}
+
+int inputValidator(string entry)
+{
+    int number = 0;
+    bool isValid = false;
+    do
+    {
+        Console.Write($"Enter {entry} number: ");
+        readInput = Console.ReadLine();
+
+        if (readInput != null)
+        {
+            isValid = int.TryParse(readInput, out number);
+        }
+    } while (!isValid);
+
+    return number;
+}
---
+++ StringSquare/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter string: ");
+string? readInput = Console.ReadLine();
+string reverseString = "";
+
+if (readInput != null)
+{
+    for (int left = 1; left < readInput.Length - 1; left++)
+    {
+        Console.Write(readInput[left]);
+        for (int space = 1; space < readInput.Length - 1; space++)
+        {
+            Console.Write(" ");
+        }
+        Console.WriteLine(readInput[readInput.Length - left -1]);
+    }
+    for (int i = 0; i < readInput.Length; i++)
+    {
+        reverseString = readInput[i] + reverseString;
+    }
+    Console.WriteLine(reverseString);
+}
